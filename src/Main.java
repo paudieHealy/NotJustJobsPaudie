bb59cd8a918323334838829d44e62ede2faec78a
Paudie Healy <paudiehealy@hotmail.com>
2016-11-16 20:12:43 +0000

    Test 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        sayHi();
     }
+    public static void sayHi()
+        {
+            System.out.println("Hi");
+        }
 }
