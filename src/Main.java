b189ce635f27e744b8bb8c2bf85a4408350ab339
Paudie Healy <paudiehealy@hotmail.com>
2016-11-16 20:27:52 +0000

    added the time method

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         sayHi();
+        theTime();
     }
     public static void sayHi()
         {
             System.out.println("Hi");
+        }
+
+    public static void theTime()
+        {
+            String sec = "34";
+            String min = "23";
+            String hr = "10";
+            String result = hr + ":" + min + ":" + sec;
+            System.out.println(result);
+
         }
 }
