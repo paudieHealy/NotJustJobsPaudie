494167e62a5f017d6106b502376f682add9443bf
Paudie Healy <paudiehealy@hotmail.com>
2016-11-17 19:44:03 +0000

    Adding a linked list to keep a list of all members

--- src/Member.java
+++ src/Member.java
@@ -1,5 +1,17 @@
+import javax.lang.model.element.Element;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by paudie on 17/11/2016.
  */
 public class Member {
+
+    private List members;
+
+    public Member()
+        {
+            members = new LinkedList();
+        }
+
 }
