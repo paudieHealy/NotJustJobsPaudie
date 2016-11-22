8c948db9926a66fe76d3f5d9c0386eb3c442607a
Paudie Healy <paudiehealy@hotmail.com>
2016-11-22 00:05:42 +0000

    I made member the super class so a business could use the attributes from the member class when creating  a profile for a business.

--- src/Member.java
+++ src/Member.java
@@ -1,38 +1,74 @@
 public class Member {
 
-    private int age;
-    private Register register;
+    private String name;
+    private String county;
+    private String username;
+    private String password;
 
-    public Member(int age, String name, String county,
+    public Member()
+    {
+        this.name = "Joe Bloggs";
+        this.county = "Dublin";
+        this.username = "username";
+        this.password = "password";
+    }
+
+    public Member(String name, String county,
                   String username, String password)
         {
-            setAge(age);
-            setRegister(new Register(name,county,username,password));
+            setName(name);
+            setCounty(county);
+            setUsername(username);
+            setPassword(password);
+
         }
 
-    public int getAge()
+    public String getName()
         {
-            return age;
+            return name;
         }
 
-    public void setAge(int age)
+    public void setName(String name)
         {
-            this.age = age;
+            this.name = name;
         }
 
-    public Register getRegister()
-        {
-            return register;
-        }
+    public String getCounty()
+    {
+        return county;
+    }
 
-    public void setRegister(Register register)
-        {
-            this.register = register;
-        }
+    public void setCounty(String county)
+    {
+        this.county = county;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+
 
     public String toString()
         {
-            return getRegister() + "\nMember's age: " + getAge();
+            return "\nName: " + getName() + "\nCounty: " + getCounty() +
+                    "\nUsernaem: " + getUsername() + "Password: " + getPassword();
         }
 
 }
