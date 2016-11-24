18e62ad261134f9da5e448065a1b86b52139b84c
Paudie Healy <paudiehealy@hotmail.com>
2016-11-24 23:34:24 +0000

    Type safety added to the registration so that the user inputs valid information when registering a member or a business

--- src/RegisterMember.java
+++ src/RegisterMember.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class RegisterMember {
 
     public RegisterMember()
-            
+
     {
         List<Member> member = new ArrayList<Member>();
 
@@ -40,11 +40,38 @@ public class RegisterMember {
         String county;
         String username;
         String password;
+        int yearOfBirth;
         name = JOptionPane.showInputDialog("Please enter your name: ");
+            if(name.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    name = JOptionPane.showInputDialog("Please enter your name: ");
+                }
         county = JOptionPane.showInputDialog("Please enter your county: ");
+            if(county.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    county = JOptionPane.showInputDialog("Please enter your county: ");
+                }
         username = JOptionPane.showInputDialog("Please enter your username: ");
+            if(username.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    username = JOptionPane.showInputDialog("Please enter your username: ");
+                }
         password = JOptionPane.showInputDialog("Please enter your password: ");
-        Member member = new Member(name,county,username,password);
+            if(password.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    password = JOptionPane.showInputDialog("Please enter your password: ");
+                }
+        yearOfBirth = Integer.parseInt(JOptionPane.showInputDialog("Please enter year of birth: "));
+            if(password.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    Integer.parseInt(JOptionPane.showInputDialog("Please enter year formed: "));
+                }
+        Member member = new Member(name,county,username,password, yearOfBirth);
         return member;
     }
 
@@ -55,12 +82,45 @@ public class RegisterMember {
         String county;
         String username;
         String password;
+        int yearOfBirth;
         companyName = JOptionPane.showInputDialog("Please enter your company name: ");
+            if(companyName.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    companyName = JOptionPane.showInputDialog("Please enter your company name: ");
+                }
         name = JOptionPane.showInputDialog("Please enter your name: ");
+            if(name.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    name = JOptionPane.showInputDialog("Please enter your name: ");
+                }
         county = JOptionPane.showInputDialog("Please enter your county: ");
+            if(county.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    county = JOptionPane.showInputDialog("Please enter your county: ");
+                }
         username = JOptionPane.showInputDialog("Please enter your username: ");
+            if(username.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    username = JOptionPane.showInputDialog("Please enter your username: ");
+                }
         password = JOptionPane.showInputDialog("Please enter your password: ");
-        Business business = new Business(companyName,name,county,username,password);
+            if(password.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    password = JOptionPane.showInputDialog("Please enter your password: ");
+                }
+        yearOfBirth = Integer.parseInt(JOptionPane.showInputDialog("Please enter year formed: "));
+            if(password.equals(""))
+                {
+                    JOptionPane.showConfirmDialog(null,"Please enter valid information!!");
+                    Integer.parseInt(JOptionPane.showInputDialog("Please enter year formed: "));
+                }
+
+        Business business = new Business(companyName,name,county,username,password, yearOfBirth);
         return business;
 
     }
