d9b488a6fab288992d2c5d2de68be977a9decce7
Paudie Healy <paudiehealy@hotmail.com>
2016-12-09 14:24:32 +0000

    Final Business Class

--- src/Business.java
+++ src/Business.java
@@ -2,26 +2,29 @@ public class Business extends Member{
 
     private String companyName;
     private Member member;
-    private double companyRate;
-    private Provence provence;
-    private WorkType work;
+    private String provence;
+    private String workType;
 
     public Business(String companyName,String name, String county,
-                    String username, String password, int yearOfBirth,Provence provence,WorkType work)
+                    String username, String password, int yearOfBirth, String provence, String workType)
     {
         super(name,county,username,password, yearOfBirth);
         this.provence = provence;
-        this.work = work;
+        this.workType = workType;
         this.companyName = companyName;
     }
 
-    public Provence getProvence(){
+    public String getProvence(){
         return provence;
     }
 
-    public WorkType getWorkType(){
-        return work;
+    public void setProvence(String provence) { this.provence = provence; }
+
+    public String getWorkType(){
+        return workType;
     }
+
+    public void setWorkType(String workType) { this.workType = workType; }
 
     public String getCompanyName()
     {
@@ -41,6 +44,12 @@ public class Business extends Member{
     public void setMember(Member member)
     {
         this.member = member;
+    }
+
+    public String toString()
+    {
+        return "\n\nCompany Name: " + getCompanyName() + super.toString() +
+                "\nProvence: " + getProvence() + "\nWork Type: " + getWorkType();
     }
 
 }
